4d9664276e138cc8b6b516cdb8c5b6f33f581eac
YoavShef <iyoavsheffer@gmail.com>
2026-09-17 16:07:11 +0300

    Add project files.

---
+++ Project Y4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Y4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time;
+            int hours, minutes;
+
+            Console.WriteLine("ENTER A NUMBER:");
+            time = int.Parse(Console.ReadLine());
+
+            hours = time / 100;
+            minutes = time % 100;
+            Console.WriteLine("the time is" + hours + ":"  + minutes);
+        }
+    }
+}
